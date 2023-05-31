c32ee238bc2a7e324d71d3cd8073b8a2f10c9496
Moussa Mokhtari <poolgazal@gmail.com>
2023-05-31 13:45:43 +0300

    initial commit

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+import { Plugin, DefineComponent } from 'vue';
+
+export const RecycleScroller: Exclude<Plugin['install'], undefined> | DefineComponent;
+export const DynamicScroller: Exclude<Plugin['install'], undefined> | DefineComponent;
+export const DynamicScrollerItem: Exclude<Plugin['install'], undefined> | DefineComponent;
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'Vue3RecycleScroller',
+      // the proper extensions will be added
+      fileName: 'vue3-virtual-scroller',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue(), ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
